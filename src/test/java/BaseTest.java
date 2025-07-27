8542cfe4a0b3caba4493de95efc96136207dc940
Taredel <taredel@mail.ru>
2025-07-27 20:18:58 +0200

    selenium init

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 public class BaseTest {
+
+    WebDriver webDriver;
+
+    @BeforeSuite
+    public void initDriver() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+    }
+
+    @Test
+    public void googleTest() {
+        webDriver.get("https://google.com");
+    }
 }
